1c9cdf8e8ed04947b1d7f6c50ef28806455d4e76
Hoàng Minh Hiển <hien.hm@atomsolution.com.vn>
2024-09-17 00:27:08 +0700

    feat: set up packages and clear source

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ["var(--font-kongtext)"],
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        hop: {
+          "0%, 100%": { transform: "translateY(-1.5%)" },
+          "50%": { transform: "translateY(1.5%)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle 0.75s linear infinite",
+        hop: "hop 0.55s ease-in infinite",
       },
     },
   },
